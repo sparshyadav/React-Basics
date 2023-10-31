e1ed03ea27fc4fe9e33b374494d89f4837bbae8a
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-31 14:46:32 +0530

    Updated and Formatted Code

--- top-course-starter/src/App.js
+++ top-course-starter/src/App.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
-import {apiUrl, filterData} from "./data";
+import { apiUrl, filterData } from "./data";
 import Navbar from "./components/Navbar";
 import Cards from "./components/Cards";
 import Filter from "./components/Filter";
 // import { toast } from "react-toastify/dist/components";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 const App = () => {
 
-  const [courses, setCourses]=useState(null);
+  const [courses, setCourses] = useState(null);
 
-  async function fetchData(){
-    try{
-      let response=await fetch(apiUrl);
-      let data=await response.json();
+  async function fetchData() {
+    try {
+      let response = await fetch(apiUrl);
+      let data = await response.json();
       setCourses(data);
     }
-    catch(error){
+    catch (error) {
       toast.error("Problem with Network");
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchData();
   })
 
