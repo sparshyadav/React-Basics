99f9f02eca789f30cd8eb5d142451599efa93131
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-10-31 11:29:17 +0530

    Using event.target.value

---
+++ topcourses/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, {useEffect, useState} from 'react';
+
+function App() {
+  const [text, setText]=useState('');
+
+
+
+  function changeHandler(event){
+    console.log(text);
+    setText(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input type='text' onChange={changeHandler}></input>
+    </div>
+  );
+}
+
+export default App;
