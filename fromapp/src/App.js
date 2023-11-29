4c2a3e063687f01d380b9de4922bc474c4e41230
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-29 15:09:48 +0530

    Updated Repository

--- fromapp/src/App.js
+++ fromapp/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({ firstName: "", lastName: "", email: "" });
+
+
+  function changeHandler(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value
+      }
+    });
+    console.log(formData.firstName);
+  }
+
   return (
     <div className="App">
-      
+      <form>
+        <input type='text' placeholder='FirstName' name='firstName' onChange={changeHandler} value={formData.firstName} />
+        <input type='text' placeholder='LastName' name='lastName' onChange={changeHandler} value={formData.lastName} />
+        <input type='email' placeholder='Email' name='email' onChange={changeHandler} value={formData.email} />
+      </form>
     </div>
   );
 }
